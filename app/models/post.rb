4ecc0c11528716bd4e4347dfc262c3b673407aaf
sudh rk <sudh@sudhs-Mac.local>
2016-08-26 17:15:22 +0530

    image bugfixes 1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
+  has_many :comments, :dependent => :destroy
   validates_presence_of :content
   has_many :pictures, :dependent => :destroy
 end
